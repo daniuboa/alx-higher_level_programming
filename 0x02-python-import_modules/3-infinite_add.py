0c8503ef6bf226969aa87de83161d1fe69741b46
Dani Uboa <uboadaniel@gmail.com>
2022-06-30 13:24:22 +0300

    Made changes

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,10 +3,7 @@ from ast import add
 from audioop import add
 
 if __name__ == "__main__":
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
     import sys
 
     total = 0
@@ -14,20 +11,16 @@ if __name__ == "__main__":
         total += int(sys.argv[i + 1])
 
     print(f"{total}")
-=======
->>>>>>> 72209289b2beb530fec0da42b46bbc2f6874ff32
+
     from sys import argv
     addition = 0
 
     number_of_args = len(argv)
 
-<<<<<<< HEAD
     if number_of_args == 0:
         print(f"{addition}")
     for argument in argv:
         if argument != argv[0]:
             addition += int(argument)
-=======
+
     print(f"total")
->>>>>>> 22b61288cde4391e060b5cef42a1d50039f2265b
->>>>>>> 72209289b2beb530fec0da42b46bbc2f6874ff32

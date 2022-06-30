233623374078ff20c7441cab3ae55a994b7b9578
Dani Uboa <uboadaniel@gmail.com>
2022-06-30 13:20:06 +0300

    3-infinite_add.py correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+from ast import add
+from audioop import add
 
 if __name__ == "__main__":
+
     from sys import argv
+    addition = 0
 
-    total = 0
-    for i in range(len(argv) - 1):
-        total += int(argv[i + 1])
+    number_of_args = len(argv)
 
-    print(f"total")
+    if number_of_args == 0:
+        print(f"{addition}")
+    for argument in argv:
+        if argument != argv[0]:
+            addition += int(argument)

5f3804eba2dac6e27f68f809be29c4bded848e69
Dani Uboa <uboadaniel@gmail.com>
2022-06-30 15:19:59 +0300

    2-args.py changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-import sys
-
-def main():
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 arguments.")
-        print(f"1. {sys.argv[1]}")
-    else:
-        print(f"{argc} arguments.")
-        for i in range(1, argc + 1):
-            print(f"{i}: {sys.argv[i]}")
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

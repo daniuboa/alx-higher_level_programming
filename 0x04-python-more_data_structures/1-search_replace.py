d10a03c30bdec5a2c952d09546aff97d65012274
Dani Uboa <uboadaniel@gmail.com>
2022-07-06 20:14:53 +0300

    Corrections

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i, item in enumerate(new_list):
-        if item == search:
-            new_list[i] == replace
-
-    return (new_list)
+    return list(map(lambda x: replace if x == search else x, my_list))

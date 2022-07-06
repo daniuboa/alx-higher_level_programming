2b2626d384c3f849059e42ae42ee03c8d26f804e
Dani Uboa <uboadaniel@gmail.com>
2022-07-06 21:22:16 +0300

    Corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    current_high_score = 0
-    high_score_holder = None
-
-    for key in a_dictionary:
-        if a_dictionary[key] > current_high_score:
-            current_high_score = a_dictionary[key]
-            high_score_holder = key
-
-    return high_score_holder
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return ret

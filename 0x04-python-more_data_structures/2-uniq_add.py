551b07d64da95008b9758ad0d047b690ea66692e
Dani Uboa <uboadaniel@gmail.com>
2022-07-06 20:24:05 +0300

    Corrections

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-from unittest import result
-
-
 def uniq_add(my_list=[]):
+
+    new_list = list(set(my_list))
     result = 0
-    for x in set(my_list):
+    for x in new_list:
         result += x
 
     return (result)

81f86925be5b3390b88d60ab10987286d7923ce4
Daniel Uboa <uboadaniel@gmail.com>
2022-08-03 14:09:57 -0700

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -12,4 +12,4 @@ def from_json_string(my_str):
     """
     # print("type json.loads(my_str)--> {}".format(type(json.loads(my_str))))
     # print("type my_str--> {}".format(type(my_str)))
-    return json.loads(my_str)    
+    return json.loads(my_str)

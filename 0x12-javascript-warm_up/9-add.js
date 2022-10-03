efbf4431e022515c56475289f3094edfebe721d2
Daniel Uboa <51877907+daniuboa@users.noreply.github.com>
2022-10-03 23:09:56 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 function add (a, b) {
-    console.log(parseInt(a) + parseInt(b));
-  }
-  add(process.argv[2], process.argv[3]);
+  return a + b;
+}
+console.log(add(a, b));

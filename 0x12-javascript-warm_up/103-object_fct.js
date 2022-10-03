8f48a877245e0e0f54455112248df9efe32af678
Dani Uboa <uboadaniel@gmail.com>
2022-10-03 22:50:47 +0300

    update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,16 +2,16 @@
 let myObject = {
     type: 'object',
     value: 12
-};
-console.log(myObject);
-
-myObject.incr = function () {
+  };
+  console.log(myObject);
+  
+  myObject.incr = function () {
     this.value++;
-};
-
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+  };
+  
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);

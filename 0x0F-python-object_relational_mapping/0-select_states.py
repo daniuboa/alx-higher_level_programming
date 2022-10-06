0f72ede359665207187c1cc5d9ed2d66f3b6d1cf
daniuboa <uboadaniel@gmail.com>
2022-10-06 12:43:00 -0700

    create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa.
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connet(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `STATES`")
+    [print(state) for state in c.fetchall()]

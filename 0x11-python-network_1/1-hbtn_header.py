3c1d6ca6a0527ad01af0e959a706c18feb30eef7
Daniel Uboa <uboadaniel@gmail.com>
2022-10-21 12:54:03 +0300

    update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and
-displays the value of the X-Requested-d variable found in the header of the response..
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the X-Requested-d variable found in the header of the response.
 """
 import urllib.request
 import sys
